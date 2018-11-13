29111cfb50b4a918c369354737789a33acdecac7
wangbiqiang <27292369511@qq.com>
2018-11-13 17:58:52 +0800

    #BAP-323#

---
+++ src/main/java/cn/com/study/boot/config/PrimaryJpaConfig.java
@@ -0,0 +1,83 @@
+package cn.com.study.boot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.mysql.fabric.jdbc.FabricMySQLDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+/**
+ * 一个数据库的情况，如果有两个数据库，则复制当前类，修改名称即可
+ *
+ * @author Administrator
+ */
+@Configuration
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "primaryEntityManagerFactory",
+        transactionManagerRef = "primaryTransactionManager",
+        basePackages = "cn.com.study.**.dao"
+)
+@EnableTransactionManagement
+@EnableConfigurationProperties(JpaProperties.class)
+public class PrimaryJpaConfig {
+
+    private final DataSource primaryDS;
+    private final JpaProperties jpaProperties;
+
+    @Autowired
+    public PrimaryJpaConfig(JpaProperties jpaProperties, @Qualifier("primaryDS") DataSource primaryDS) {
+        this.jpaProperties = jpaProperties;
+        this.primaryDS = primaryDS;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConditionalOnMissingClass("com.alibaba.druid.pool.DruidDataSource")
+    public DataSource dataSource(){
+        return new FabricMySQLDataSource();
+    }
+
+    @Bean("primaryDS")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource primaryDataSource(){
+        return new DruidDataSource();
+    }
+
+    @Primary
+    @Bean(name = "primaryEntityManager")
+    public EntityManager primaryEntityManager(EntityManagerFactoryBuilder builder){
+        return primaryEntityManagerFactory(builder).getObject().createEntityManager();
+    }
+
+    @Primary
+    @Bean(name = "primaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean primaryEntityManagerFactory(EntityManagerFactoryBuilder builder){
+        return builder.dataSource(primaryDS)
+                .properties(jpaProperties.getHibernateProperties(primaryDS))
+                .packages("cn.com.study.**.po")
+                .persistenceUnit("primaryPersistenceUnit")
+                .build();
+    }
+
+    @Bean("primaryTransactionManager")
+    public PlatformTransactionManager primaryTransactionManager(EntityManagerFactoryBuilder builder){
+        return new JpaTransactionManager(primaryEntityManagerFactory(builder).getObject());
+    }
+
+}
--- src/main/java/cn/com/study/boot/config/WebConfiguration.java
+++ src/main/java/cn/com/study/boot/config/WebConfiguration.java
@@ -1,11 +1,28 @@
 package cn.com.study.boot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableWebMvc
 @EnableSpringDataWebSupport
 public class WebConfiguration {
+    /**
+     *
+     * @return
+     */
+    @Bean
+    public Executor getExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.initialize();
+        return executor;
+    }
 }
